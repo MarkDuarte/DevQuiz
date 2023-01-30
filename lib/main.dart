e37b693532a9b22b188761d27825f619b40ad114
Marcos Duarte <marcosduarte1994@gmail.com>
2023-01-30 16:27:07 -0300

    feat: new widgets at home

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
-import 'package:flutter/material.dart';
+import 'package:DevQuiz/core/core.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'DevQuiz',
-    )
-  }
+  runApp(AppWidget());
 }
